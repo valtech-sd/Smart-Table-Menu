c6147eadb725f7d71f195530cea7261c3bb4937c
Eric Lifs <lifszyc.eric@gmail.com>
2022-08-18 17:19:27 -0300

    feat: update cart structure

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,7 +1,7 @@
 import { useCart } from "react-use-cart";
 
 export const Cart = () => {
-  const { isEmpty, totalUniqueItems, updateItemQuantity, items } = useCart();
+  const { isEmpty, totalItems, items } = useCart();
 
   if (isEmpty)
     return (
@@ -12,18 +12,11 @@ export const Cart = () => {
 
   return (
     <div className="cart">
-      <div className="cart__item cart__title">Cart ({totalUniqueItems})</div>
-
+      <div className="cart__item cart__title">Cart ({totalItems})</div>
       <ul>
         {items.map((item) => (
           <li key={item.id} className="cart__item">
-            {item.quantity} x {item.name} &mdash;
-
-            <div className="cart__btn__container">
-                <button className="cart__control__btn" onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
-                <span className="cart__counter__output">{item.quantity}</span>
-                <button className="cart__control__btn" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
-            </div>
+            {item.quantity} x {item.name}
           </li>
         ))}
       </ul>

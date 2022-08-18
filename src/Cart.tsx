ec86a1f1e779dbc7ac925d8f88a48cdeb297e1e0
Gonzalo Zunino <gonzalo.zunino@gmail.com>
2022-08-18 16:43:05 -0300

    Align counter for cart

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -13,12 +13,11 @@ export const Cart = () => {
   return (
     <div className="cart">
       <div className="cart__item cart__title">Cart ({totalUniqueItems})</div>
-
-      <ul>
+      <div className="divider" />
+      <ul className="cart__section">
         {items.map((item) => (
           <li key={item.id} className="cart__item">
-            {item.quantity} x {item.name} &mdash;
-
+            {item.name} &mdash;
             <div className="cart__btn__container">
                 <button className="cart__control__btn" onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
                 <span className="cart__counter__output">{item.quantity}</span>
